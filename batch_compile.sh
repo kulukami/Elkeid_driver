055b7ecae49acf580bcc5387693497994d2d7cda
Bach <kulukami@gmail.com>
2022-04-29 15:57:34 +0800

    update rhel7

---
+++ batch_compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir -p /ko_output
+KO_NAME=$(grep "MODULE_NAME" ./LKM/Makefile | grep -m 1 ":=" | awk '{print $3}')
+UBUNTU_OR_DEBIAN_FLAG=$(cat /etc/*release | grep -iE "ubuntu|debian")
+FLAG_SIZE=${#UBUNTU_OR_DEBIAN_FLAG}
+
+if [ $FLAG_SIZE -ne 0 ]; then
+    echo "this is ubuntu or debian"
+    FILES=/lib/modules/*
+else
+    echo "this is centos"
+    FILES=/usr/src/kernels/*
+fi
+
+for f in $FILES
+do
+    set -e
+    set -o xtrace
+    KV="$(basename -- $f)"
+    echo "Processing $KV file..."
+    KVERSION=$KV  make -C ./LKM clean || true
+    BATCH=true KVERSION=$KV   make  -C ./LKM -j all || true
+    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true
+    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true
+    KVERSION=$KV  make -C ./LKM clean || true
+done
