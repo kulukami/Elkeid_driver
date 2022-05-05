5df41f7a998f7cdd3800720be3d093b37586cd54
Bach <kulukami@gmail.com>
2022-05-05 09:45:29 +0800

    add clean disk

--- batch_compile.sh
+++ batch_compile.sh
@@ -7,21 +7,32 @@ FLAG_SIZE=${#UBUNTU_OR_DEBIAN_FLAG}
 
 if [ $FLAG_SIZE -ne 0 ]; then
     echo "this is ubuntu or debian"
-    FILES=/lib/modules/*
+    for f in /lib/modules/*
+    do
+        set -e
+        set -o xtrace
+        KV="$(basename -- $f)"
+        echo "Processing $KV file..."
+        KVERSION=$KV  make -C ./LKM clean || true
+        BATCH=true KVERSION=$KV   make  -C ./LKM -j all || true
+        sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true
+        mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true
+        KVERSION=$KV  make -C ./LKM clean || true
+    done
 else
     echo "this is centos"
-    FILES=/usr/src/kernels/*
+    for f in /usr/src/kernels/*
+    do
+        set -e
+        set -o xtrace
+        KV="$(basename -- $f)"
+        echo "Processing $KV file..."
+        KVERSION=$KV  make -C ./LKM clean || true
+        BATCH=true KVERSION=$KV   make  -C ./LKM -j all || true
+        sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true
+        mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true
+        KVERSION=$KV  make -C ./LKM clean || true
+    done
 fi
 
-for f in $FILES
-do
-    set -e
-    set -o xtrace
-    KV="$(basename -- $f)"
-    echo "Processing $KV file..."
-    KVERSION=$KV  make -C ./LKM clean || true
-    BATCH=true KVERSION=$KV   make  -C ./LKM -j all || true
-    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true
-    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true
-    KVERSION=$KV  make -C ./LKM clean || true
-done
+
