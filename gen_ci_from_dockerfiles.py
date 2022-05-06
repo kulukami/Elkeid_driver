24aaed0568ec0a6dcbf0c3c8f168df0ad5d3c793
Bach <kulukami@gmail.com>
2022-05-06 18:12:59 +0800

    add tlinux

--- gen_ci_from_dockerfiles.py
+++ gen_ci_from_dockerfiles.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 all_dockers = os.listdir("dockerfiles")
 
-black_list = ["rhel7_elrepo", "rhel8_elrepo"]
+black_list = ["rhel7_elrepo", "rhel8_elrepo", "rhel6"]
 
 all_vms = []
 
